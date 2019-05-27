88881d5eea487e9fc8767efa2d098d0d9ea03836
taeinchoi9166 <taein1009@naver.com>
2019-05-27 13:00:11 +0900

    dev - 0.0.2

--- app/src/main/java/ga/sproutseed/mds/magicdecksharer/HomeActivity.java
+++ app/src/main/java/ga/sproutseed/mds/magicdecksharer/HomeActivity.java
@@ -1,16 +1,24 @@
 package ga.sproutseed.mds.magicdecksharer;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class HomeActivity extends AppCompatActivity {
-
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout navDrawer;
+    private NavigationView navView;
+    ActionBarDrawerToggle drawerToggle;
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +34,20 @@ public class HomeActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setTitle("HEEEEEEllow!!!");
+        ab.setDisplayHomeAsUpEnabled(true);
+        ab.setHomeAsUpIndicator(R.drawable.ic_menu_white);
+
+//        navDrawer = findViewById(R.id.nav_drawer);
+//        navView = findViewById(R.id.nav_view);
+//        drawerToggle = new ActionBarDrawerToggle(this,navDrawer,toolbar,R.string.open_drawer,R.string.close_drawer);
+//        navDrawer.addDrawerListener(drawerToggle);
+//        navView.setNavigationItemSelectedListener(this);
+//
+//        navDrawer.openDrawer(navView);
     }
 
     @Override
@@ -48,5 +70,10 @@ public class HomeActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return false;
     }
 }
